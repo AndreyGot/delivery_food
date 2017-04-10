43dde3fbbe9ecda87bd8761ef9f1307312412ea1
AlexandrFedorenko <alexandrfedorenkoooo@gmail.com>
2017-04-10 09:42:29 +0300

    v0.1 frontend 10.04.2017

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!-- Favicon and Touch Icons-->
+    <link rel="shortcut icon" href="{{asset('img/favicon.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('img/favicon.png')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{asset('img/favicon.png')}}">
+    <!-- /Favicon-->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -28,7 +32,6 @@
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-
                     <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
@@ -36,25 +39,28 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                    {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
+                        {{--{{ config('app.name', 'Laravel') }}--}}
+                    {{--</a>--}}
+                    <a href="{{ url('/') }}" class="navbar-brand">
+                        <img src="{{ asset('img/logo.png') }}">
                     </a>
                 </div>
-
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+
+                        <form class="main-header_form live-search-box">
+                            <input type="text" name="text" class="search live-search" autocomplete="off" placeholder="Поиск">
+                        </form>
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         @if (Auth::guest())
 
-                            <li><a href="{{ route('admin_login_form') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
+                            <li><a class="btn btn--enter" href="{{ route('admin_login_form') }}">Вход</a></li>
+                            <li><a class="btn btn--reg" href="{{ url('/register') }}">Регистрация</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -82,22 +88,48 @@
                 </div>
             </div>
         </nav>
+        <div class="main-header_bottom">
+            <div class="container">
+                <div class="header_text">
+                    Куда доставить еду?
+                </div>
+                <form class="header_box" onsubmit="return inputedAddress();">
+                    <div class="header_city tooltip">
+                        <a  class="tooltip_title"  id="current-city" >Сочи</a>
 
-    </div>
-        <div>
-            <p>Главная страница</p>
+                    </div>
+                    <div class="header_street">
+                        <input name="street" tabindex="1" placeholder="Укажите улицу" data-value="" value="" type="text">
+                    </div>
+                    <div class="header_house">
+                        <input name="house" tabindex="2" placeholder="Дом" data-value="" value="" type="text">
+                    </div>
+                    <button type="submit" class="header_button btn">Найти рестораны</button>
+                </form>
+            </div>
         </div>
+    </div>
+       {{--dop menu--}}
+
 
         @yield('content') 
         {{--
         @include('shop.tamplates.howto')
         --}}
+
         @include('shop.tamplates.footer')
         @include('shop.tamplates.cart_panel')
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/library.js') }}"></script>
     <script src="{{ asset('js/script_for_exspres.js') }}"></script>
+    <script src="{{ asset('js/jquery.maskedinput.js') }}"></script>
+    <script>
+        jQuery(function($){
+            $("#phone").mask("+7(999) 999-99-99",{placeholder:" "});
+        });
+    </script>
 </body>
 </html>
--- resources/views/shop/restaurant/listRestaurant.blade.php
+++ resources/views/shop/restaurant/listRestaurant.blade.php
@@ -1,18 +1,74 @@
 <div class="catalog container">
-    <div class="new-header"><i class="sprite sprite-catalog"></i> Выберите категорию</div>
-    <div class="catalog_container row row--inline">
-        @foreach ($restaurants as $restaurant)
-        <div class="col-xs-3">
-            <div class="catalog-item">
-                <h2>{{ $restaurant->name }}</h2>
-                <img src="{{ asset($restaurant->image) }}">
-                <p>Описание: {{ $restaurant->description }}</p>
-                <p>Рабочее время: {{ $restaurant->working_hours }}</p>
-                <p>Рейтинг {{ $restaurant->rating }}</p>
-                <a class ="btn btn-info" href="{{route('shop_restaurant_show',[$restaurant])}}" 
-                    role ="button">Перейти к ресторану</a>
+    <div class="new-header"><i class="sprite sprite-catalog"></i> Выберите ресторан</div>
+    {{--<div class="catalog_container row row--inline">--}}
+        {{--@foreach ($restaurants as $restaurant)--}}
+        {{--<div class="col-xs-3">--}}
+            {{--<div class="catalog-item">--}}
+                {{--<h2>{{ $restaurant->name }}</h2>--}}
+                {{--<img src="{{ asset($restaurant->image) }}">--}}
+                {{--<p>Описание: {{ $restaurant->description }}</p>--}}
+                {{--<p>Рабочее время: {{ $restaurant->working_hours }}</p>--}}
+                {{--<p>Рейтинг {{ $restaurant->rating }}</p>--}}
+                {{--<a class ="btn btn-info" href="{{route('shop_restaurant_show',[$restaurant])}}" --}}
+                    {{--role ="button">Перейти к ресторану</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--@endforeach--}}
+    {{--</div>--}}
+    <a href="restaurants/menu/tanukimsk.html" onclick="return orgClick();"
+       class="restoran-item row restoran-item--food-best h-tooltip">
+        <div class="col s-3">
+            <figure class="restoran-item_image">
+                <img src="https://cdn.zakazaka.ru/db/181/173/image.c.178506.jpg" height="160" width="160"
+                     alt="Тануки">
+            </figure>
+        </div>
+        <div class="col s-9">
+            <div class="restoran-item_top row">
+                <div class="col s-8">
+                    <h4 class="restoran-item_title">Тануки</h4>
+                    <p class="restoran-item_description">
+                        <span>Кухня:</span> Суши / Китайская / Японская
+                    </p>
+                </div>
+                <div class="col s-4 text-right ">
+                    <div class="rate rate--4--5">
+                        <i></i><i></i><i></i><i></i><i></i>
+                    </div>
+                    <p class="restoran-item_star-col">992 оценки</p>
+                </div>
+            </div>
+            <div class="restoran-item_bottom row">
+                <div class="col s-3">
+                    <p class="restoran-item_sub-titile">Мин. заказ:</p>
+                    <p class="restoran-item_big"><i class="sprite sprite-ico-stack "></i>
+                        1000 P
+                    </p>
+                </div>
+                <div class="col s-3">
+                    <p class="restoran-item_sub-titile">Рабочее время:</p>
+                    <p class="restoran-item_big"><i class="sprite sprite-ico-rocket-w"></i>
+                        10:00-18:00
+
+                    </p>
+                </div>
+                <div class="col s-3">
+                    <p class="restoran-item_sub-titile">Время доставки</p>
+                    <p class="restoran-item_big"><i class="sprite sprite-ico-timer-2"></i> До 1 часа</p>
+                </div>
+                <div class="col s-3">
+                    <p class="restoran-item_sub-titile">Оплата картой:</p>
+                    <p class="restoran-item_big"><i class="sprite sprite-ico-viza"></i> Есть</p>
+                </div>
             </div>
         </div>
-        @endforeach
+
+
+        <div class="b-tooltip to">F1 рекомендует этот ресторан</div>
+
+    </a>
+    <div class="index-control">
+        <a href="index.html#" class="js-tab-control active" data-tab="js-tab-tovar">Доставка еды</a>
+        <a href="index.html#" class="js-tab-control" data-tab="js-tab-product">Продукты на дом</a>
     </div>
 </div>
