23b98339898bf1648a855f4c20a72673a4835bb5
AlexandrFedorenko <alexandrfedorenkoooo@gmail.com>
2017-04-13 11:58:40 +0300

    add all index page

--- resources/views/shop/tamplates/cart_panel.blade.php
+++ resources/views/shop/tamplates/cart_panel.blade.php
@@ -1,28 +1,31 @@
 <div class="cart-pane " data-score="">
-    <div class="container row">
-        <a href="cart.html" class="col-xs-2 cart-pane__logo">
-            <i class="sprite sprite-ico-cart-footer"></i> Корзина
-        </a>
-        <div class="col-xs-7 text-center">
-            <div class="cart-pane__item">
-                БЛЮД В КОРЗИНЕ: <div class="cart-pane__number">0</div>
-            </div>
-            <div class="cart-pane__item">
-                <div class="tooltip">
-                    <div class="js-toggle-tooltip">
-                        ИЗБРАННЫЕ ЗАВЕДЕНИЯ <div class="cart-pane__number">0</div>
-                    </div>
-                    <div class="tooltip_content tooltip_content--medium tooltip_content--footer">
-                        <div class="scroll"></div>
+    <div class="container">
+        <div class="row">
+            <a href="#" class="col-md-2 col-sm-2 cart-pane__logo">
+                <i class="sprite sprite-ico-cart-footer"></i> Корзина
+            </a>
+            <div class="col-md-7 col-sm-7 text-center">
+                <div class="cart-pane__item">
+                    БЛЮД В КОРЗИНЕ: <div class="cart-pane__number">0</div>
+                </div>
+                <div class="cart-pane__item">
+                    <div class="tooltip">
+                        <div class="js-toggle-tooltip">
+                            ИЗБРАННЫЕ ЗАВЕДЕНИЯ <div class="cart-pane__number">0</div>
+                        </div>
+                        <div class="tooltip_content tooltip_content--medium tooltip_content--footer">
+                            <div class="scroll"></div>
+                        </div>
                     </div>
                 </div>
+                <div class="cart-pane__item">
+                    НА СУММУ <div class="cart-pane__sum">0 Р</div>
+                </div>
             </div>
-            <div class="cart-pane__item">
-                НА СУММУ <div class="cart-pane__sum">0 Р</div>
+            <div class="col-md-3 col-sm-3 text-right">
+                <a href="{{route('shop_show_cart')}}" class="btn btn--orange">Оформить заказ</a>
             </div>
         </div>
-        <div class="col-xs-3 text-right">
-            <a href="{{route('shop_show_cart')}}" class="btn btn--orange">Оформить заказ</a>
-        </div>
+
     </div>
 </div>
