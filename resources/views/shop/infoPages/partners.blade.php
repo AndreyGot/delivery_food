bf119a23e7513c7cc2194173fe26e81ea1676fad
AlexandrFedorenko <alexandrfedorenkoooo@gmail.com>
2017-05-15 12:02:15 +0300

    fix all pages adaptive and add page 404 & 500

--- resources/views/shop/infoPages/partners.blade.php
+++ resources/views/shop/infoPages/partners.blade.php
@@ -24,29 +24,51 @@
 				</ul>
 			</div>
 		</div>
-		<a href="l#" class="btn btn--green btn--padd advantages_btn">заполнить анкету для сотрудничества</a>
+		<a href="#"  data-toggle="modal" data-target="#myModal" class="btn btn--green btn--padd advantages_btn">заполнить анкету для сотрудничества</a>
 	</div>
 </section>
 
-
-		<!-- TODO Модальное окно - Анкета для партнеров -->
-<div class="modal modal-partners">
-	<div class="modal_wrapper">
-		<div class="modal_container">
-			<a href="partners.html#" class="modal_close js-close-modal">Закрыть окно</a>
-			<form onsubmit="return sendPartnerForm();">
-				<p class="modal_title">Анкета для партнеров</p>
-				<input name="title" required type="text" class="custom-field" placeholder="Фирменное название">
-				<input name="url" type="text" class="custom-field" placeholder="http://">
-				<input name="city" required type="text" class="custom-field" placeholder="Город или область">
-				<input name="name" required type="text" class="custom-field" placeholder="Контактное лицо">
-				<input name="phone" required type="tel" class="custom-field" placeholder="Телефон">
-				<input name="email" required type="email" class="custom-field" placeholder="E-mail">
-				<textarea name="cmt" class="custom-field custom-field--textarea" placeholder="Комментарии"></textarea>
-				<button type="submit" class="btn btn--green custom-button">Отправить</button>
-			</form>
+<!-- Modal -->
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Анкета для партнеров</h4>
+			</div>
+			<div class="modal-body">
+				<form>
+					<input name="title" required type="text" class="custom-field" placeholder="Фирменное название">
+					<input name="url" type="text" class="custom-field" placeholder="http://">
+					<input name="city" required type="text" class="custom-field" placeholder="Город или область">
+					<input name="name" required type="text" class="custom-field" placeholder="Контактное лицо">
+					<input name="phone" required type="tel" class="custom-field" placeholder="Телефон">
+					<input name="email" required type="email" class="custom-field" placeholder="E-mail">
+					<textarea name="cmt" class="custom-field custom-field--textarea" placeholder="Комментарии"></textarea>
+					<button type="submit" class="btn btn--green custom-button">Отправить</button>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
+
+{{--<div class="modal modal-partners" id="myModalPartners">--}}
+	{{--<div class="modal_wrapper">--}}
+		{{--<div class="modal_container">--}}
+			{{--<a href="partners.html#" class="modal_close js-close-modal">Закрыть окно</a>--}}
+			{{--<form>--}}
+				{{--<p class="modal_title">Анкета для партнеров</p>--}}
+				{{--<input name="title" required type="text" class="custom-field" placeholder="Фирменное название">--}}
+				{{--<input name="url" type="text" class="custom-field" placeholder="http://">--}}
+				{{--<input name="city" required type="text" class="custom-field" placeholder="Город или область">--}}
+				{{--<input name="name" required type="text" class="custom-field" placeholder="Контактное лицо">--}}
+				{{--<input name="phone" required type="tel" class="custom-field" placeholder="Телефон">--}}
+				{{--<input name="email" required type="email" class="custom-field" placeholder="E-mail">--}}
+				{{--<textarea name="cmt" class="custom-field custom-field--textarea" placeholder="Комментарии"></textarea>--}}
+				{{--<button type="submit" class="btn btn--green custom-button">Отправить</button>--}}
+			{{--</form>--}}
+		{{--</div>--}}
+	{{--</div>--}}
+{{--</div>--}}
 
 @endsection
